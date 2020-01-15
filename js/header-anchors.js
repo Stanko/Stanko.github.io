17e5d9cd118e1ceb0c800b21c68aca5c9aa8f774
Stanko <stanko.tadic@gmail.com>
2020-01-15 15:20:43 +0100

    Removed console logs

--- js/header-anchors.js
+++ js/header-anchors.js
@@ -1,9 +1,7 @@
 const headers = document.querySelectorAll('.Post h2, .Post h3, .Post h4, .Post h5');
 
 if (headers) {
-  console.log(headers);
   headers.forEach(header => {
-    console.log(header)
     header.innerHTML =
       `<a href="#${ header.id }" class="Post-headerAnchor">
         <img src="/public/img/link.svg" />
