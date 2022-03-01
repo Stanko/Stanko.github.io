ea15470c580530c5f02ab475e9110f7eb7044cd8
Stanko <stanko.tadic@gmail.com>
2022-03-01 15:07:05 +0100

    New GH action

---
+++ build-and-deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+./zola build
+
+# Create JS file from search data
+mv ./public/search-data/index.html ./public/js/search-data.js
+rm -rf ./public/search-data/
+
+# Remove taxonomy pages
+rm -rf ./public/tags ./public/category
+
+# Remove taxonomy from the sitemap
+awk 'NR==FNR{if (/muffinman.io\/(tags|category)\/.*/) for (i=-1;i<=1;i++) del[NR+i]; next} !(FNR in del)' ./public/sitemap.xml ./public/sitemap.xml > ./public/sitemap-tmp.xml
+
+# Remove search data from the sitemap
+awk 'NR==FNR{if (/muffinman.io\/search-data/) for (i=-1;i<=1;i++) del[NR+i]; next} !(FNR in del)' ./public/sitemap-tmp.xml ./public/sitemap-tmp.xml > ./public/sitemap.xml
+
+# Remove temp
+rm ./public/sitemap-tmp.xml
+
+# Switch to gh-pages branch
+git checkout ${remote_branch}
+
+git config user.name "GitHub Actions"
+git config user.email "github-actions-bot@users.noreply.${GITHUB_HOSTNAME}"
+
+# Delete old build
+find . ! -path './.git' ! -path . ! -name 'public' -maxdepth 1 -exec rm -rf {} +
+
+# Copy everything from the new build to root
+cp -r ./public/ ./
+# Remove public folder
+rm -rf ./public
+
+# Commit with current time
+git add .
+git commit -a -m "Deploy `date +'%Y-%m-%d %H:%M:%S'`"
+
+# Push changes
+git push origin ${remote_branch}
