28be22cd1bd73e09b34b5b13e96541d97c190a3f
Stanko <stanko.tadic@gmail.com>
2025-06-03 09:35:45 +0200

    Github action

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,34 @@
 #!/bin/sh
 
-./build/brz-linux-x64
+# ./build/brz-linux-x64
 
-if [ $? -eq 0 ]
-then
-  # Switch to gh-pages branch
-  git fetch origin gh-pages gh-pages
-  git checkout gh-pages
+# if [ $? -eq 0 ]
+# then
+# Switch to gh-pages branch
+git fetch origin gh-pages gh-pages
+git checkout gh-pages
 
-  git config user.name "GitHub Actions"
-  git config user.email "github-actions-bot@users.noreply.${GITHUB_HOSTNAME}"
+git config user.name "GitHub Actions"
+git config user.email "github-actions-bot@users.noreply.${GITHUB_HOSTNAME}"
 
-  # Delete old build
-  find . ! -path './.git' ! -path . ! -name 'dist' -maxdepth 1 -exec rm -rf {} +
+# Delete old build
+find . ! -path './.git' ! -path . ! -name 'dist' -maxdepth 1 -exec rm -rf {} +
 
-  # Copy everything from the new build to root
-  cp -r ./dist/* ./
-  # Remove dist folder
-  rm -rf ./dist
+# Copy everything from the new build to root
+cp -r ./dist/* ./
+# Remove dist folder
+rm -rf ./dist
 
-  # Create .nojekyll file
-  touch .nojekyll
+# Create .nojekyll file
+touch .nojekyll
 
-  # Commit with current time
-  git add .
-  git commit -a -m "Deploy `date +'%Y-%m-%d %H:%M:%S'`"
+# Commit with current time
+git add .
+git commit -a -m "Deploy `date +'%Y-%m-%d %H:%M:%S'`"
 
-  # Push changes
-  # git push origin gh-pages
-else
-  echo "brz build failed"
-  exit 1
-fi
+# Push changes
+git push origin gh-pages
+# else
+#   echo "brz build failed"
+#   exit 1
+# fi
