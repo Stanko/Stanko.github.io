fe741cd40a9c1d87f58a26dec51315df8a355ff1
Stanko <stanko.tadic@gmail.com>
2025-08-05 12:33:36 +0200

    New projects

--- site/content/projects/index.tsx
+++ site/content/projects/index.tsx
@@ -1,10 +1,10 @@
-import { dirs } from '@brz';
-import Img from '@brz/components/img';
-import ArrowTitle from '@site/components/arrow-title';
-import Header from '@site/components/header';
-import Vertigo from '@site/components/vertigo';
-import BaseTemplate from '@site/templates/base';
-import { join } from 'node:path';
+import { dirs } from "@brz";
+import Img from "@brz/components/img";
+import ArrowTitle from "@site/components/arrow-title";
+import Header from "@site/components/header";
+import Vertigo from "@site/components/vertigo";
+import BaseTemplate from "@site/templates/base";
+import { join } from "node:path";
 
 type Project = {
   name: string;
@@ -114,20 +114,20 @@ const projects: {
   projects: Project[];
 }[] = [
   {
-    title: 'Creative coding',
+    title: "Creative coding",
     projects: [
       {
         description:
-          'My favorite hobby. I use code to create drawings, and a robot to plot them on paper. Check the art section of this website.',
-        image: './art.jpg',
-        link: '/art',
-        name: 'Generative Art',
+          "My favorite hobby. I use code to create drawings, and a robot to plot them on paper. Check the art section of this website.",
+        image: "./art.jpg",
+        link: "/art",
+        name: "Generative Art",
       },
       {
-        name: 'Creative Coding Amsterdam',
+        name: "Creative Coding Amsterdam",
         description:
-          'Since early 2024, David, Sinan and myself organize monthly meetups for anyone fascinated by creative coding.',
-        link: 'https://cca.codes/',
+          "Since early 2024, David, Sinan and myself organize monthly meetups for anyone fascinated by creative coding.",
+        link: "https://cca.codes/",
         thumb: (
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -152,104 +152,111 @@ const projects: {
       },
       {
         description:
-          'Glitchy B.A.R.D. is an experiment in robot poetry. It uses a small language model to generate crappy poems for fun and profit (I lied, there is no profit).',
-        link: 'https://muffinman.io/bard/',
-        name: 'BARD',
+          "Procedural space invaders generator for the CCA's code challenge. Made for fun and galatic domination.",
+        image: "./invader.png",
+        link: "https://muffinman.io/invaders/",
+        name: "Space Invaders Generator",
+      },
+      {
+        description:
+          "Glitchy B.A.R.D. is an experiment in robot poetry. It uses a small language model to generate crappy poems for fun and profit (I lied, there is no profit).",
+        link: "https://muffinman.io/bard/",
+        name: "BARD",
         thumb: (
           <Img
             src="./bard.png"
             alt="BARD"
             className="project__image pixel-art"
-            outputDir={join(dirs.OUTPUT, 'projects')}
-            pageDir={join(dirs.CONTENT, 'projects')}
+            outputDir={join(dirs.OUTPUT, "projects")}
+            pageDir={join(dirs.CONTENT, "projects")}
           />
         ),
       },
       {
         description:
-          'A micro creative coding playground where you can create and share animations through code.',
-        image: './pulsar.gif',
-        link: 'https://muffinman.io/pulsar/',
-        name: 'Pulsar',
+          "A micro creative coding playground where you can create and share animations through code.",
+        image: "./pulsar.gif",
+        link: "https://muffinman.io/pulsar/",
+        name: "Pulsar",
       },
       {
         description:
           'A tool that converts photos into vectors using two unique "shaders" - circular dot grid and variable-width spiral.',
-        link: 'https://muffinman.io/vertigo/',
-        name: 'Vertigo',
+        link: "https://muffinman.io/vertigo/",
+        name: "Vertigo",
         thumb: <Vertigo />,
       },
       {
         description:
-          'An interactive article on how to transform any SVG path into a vector rope drawing.',
-        image: './rope.png',
-        link: '/blog/draw-svg-rope-using-javascript/',
-        name: 'Draw SVG rope using JavaScript',
+          "An interactive article on how to transform any SVG path into a vector rope drawing.",
+        image: "./rope.png",
+        link: "/blog/draw-svg-rope-using-javascript/",
+        name: "Draw SVG rope using JavaScript",
       },
       {
         description:
-          'A custom ray marching 3D renderer that mimics hand hatching. It is using SDFs for modeling and outputs vector files. Not published yet.',
-        image: './rayven.png',
-        link: 'https://muffinman.io/blog/sneak-peek-of-rayven/',
-        name: 'Rayven',
+          "A custom ray marching 3D renderer that mimics hand hatching. It is using SDFs for modeling and outputs vector files. Not published yet.",
+        image: "./rayven.png",
+        link: "https://muffinman.io/blog/sneak-peek-of-rayven/",
+        name: "Rayven",
       },
       {
         description:
-          'One of my earliest generative projects, based on metaballs, organic-looking n-dimensional objects.',
-        image: './metaballs.png',
-        link: 'https://muffinman.io/metaballs/',
-        name: 'Metaballs',
+          "One of my earliest generative projects, based on metaballs, organic-looking n-dimensional objects.",
+        image: "./metaballs.png",
+        link: "https://muffinman.io/metaballs/",
+        name: "Metaballs",
       },
       {
         description:
           "Unfinished port of Michael Fogleman's `ln` vector 3d engine to TypeScript.",
-        image: './linea.jpg',
-        link: 'https://muffinman.io/linea',
-        name: 'Linea',
+        image: "./linea.jpg",
+        link: "https://muffinman.io/linea",
+        name: "Linea",
       },
       {
         description:
-          'My first remotely presentable generative project, utilizing a vector field to draw vibrant, neon lines.',
-        image: './neon.png',
-        link: 'https://neon.muffinman.io/#/false/15/6/60/55/140/5/2aybxq8tmqv/crdmt2f7nn8/i5g7i80cigf',
-        name: 'Neon',
-      },
-    ],
-  },
-  {
-    title: 'Hardware',
-    projects: [
-      {
-        description:
-          'Physical pixel art frame using LED matrix and ESP32, inspired by Game Frame.',
-        image: './retro-frame.png',
-        link: 'https://github.com/stanko/retro-frame',
-        name: 'Retro Frame',
+          "My first remotely presentable generative project, utilizing a vector field to draw vibrant, neon lines.",
+        image: "./neon.png",
+        link: "https://neon.muffinman.io/#/false/15/6/60/55/140/5/2aybxq8tmqv/crdmt2f7nn8/i5g7i80cigf",
+        name: "Neon",
       },
     ],
   },
   {
-    title: 'Libraries and tools',
+    title: "Hardware",
     projects: [
       {
         description:
-          'Tailor is a developer tool that simplifies inspecting spacings on websites. Available as a browser extension and as a library.',
+          "Physical pixel art frame using LED matrix and ESP32, inspired by Game Frame.",
+        image: "./retro-frame.png",
+        link: "https://github.com/stanko/retro-frame",
+        name: "Retro Frame",
+      },
+    ],
+  },
+  {
+    title: "Libraries and tools",
+    projects: [
+      {
+        description:
+          "Tailor is a developer tool that simplifies inspecting spacings on websites. Available as a browser extension and as a library.",
         thumb: <Monorail />,
-        link: 'https://muffinman.io/monorail/',
-        name: 'Monorail',
+        link: "https://muffinman.io/monorail/",
+        name: "Monorail",
       },
       {
         description:
-          'Tailor is a developer tool that simplifies inspecting spacings on websites. Available as a browser extension and as a library.',
-        image: './tailor.png',
-        link: 'https://muffinman.io/tailor/',
-        name: 'Tailor',
+          "Tailor is a developer tool that simplifies inspecting spacings on websites. Available as a browser extension and as a library.",
+        image: "./tailor.png",
+        link: "https://muffinman.io/tailor/",
+        name: "Tailor",
       },
       {
         description:
-          'Lightweight React component for animating height using CSS transitions. By far, it is my most popular library on npm.',
-        link: 'https://muffinman.io/react-animate-height/',
-        name: 'React Animate Height',
+          "Lightweight React component for animating height using CSS transitions. By far, it is my most popular library on npm.",
+        link: "https://muffinman.io/react-animate-height/",
+        name: "React Animate Height",
         thumb: (
           <div className="rah">
             <div className="rah-item"></div>
@@ -260,61 +267,68 @@ const projects: {
       },
       {
         description:
-          'Lightweight yet robust parallax library for React. It animates numerical CSS properties and colors.',
-        link: 'https://muffinman.io/react-plx/',
-        name: 'React Plx',
+          "Lightweight yet robust parallax library for React. It animates numerical CSS properties and colors.",
+        link: "https://muffinman.io/react-plx/",
+        name: "React Plx",
       },
       {
         description:
-          'Lightweight *scroll to* function with a powerful API. Scrolls window or any other DOM element.',
-        link: 'https://muffinman.io/animated-scroll-to/',
-        name: 'Animated scroll to',
+          "Lightweight *scroll to* function with a powerful API. Scrolls window or any other DOM element.",
+        link: "https://muffinman.io/animated-scroll-to/",
+        name: "Animated scroll to",
       },
       {
         description:
-          'React application boilerplate that gained popularity within the community before React Create App became a thing. The project was born from my blog post series on setting up a React project from scratch.',
-        image: './marvin.svg',
-        link: 'https://github.com/stanko/marvin/',
-        name: 'Marvin (deprecated)',
+          "React application boilerplate that gained popularity within the community before React Create App became a thing. The project was born from my blog post series on setting up a React project from scratch.",
+        image: "./marvin.svg",
+        link: "https://github.com/stanko/marvin/",
+        name: "Marvin (deprecated)",
       },
       {
         description:
-          'Minimal project setup for building and releasing npm packages.',
-        image: './keen.png',
-        link: 'https://github.com/Stanko/keen/',
-        name: 'Keen',
+          "Minimal project setup for building and releasing npm packages.",
+        image: "./keen.png",
+        link: "https://github.com/Stanko/keen/",
+        name: "Keen",
       },
       {
         description:
-          'Small CSS framework I created for my personal needs, a decade ago. Designed as a lightweight alternative to frameworks like Bootstrap. I started working on v2 recently, but I have not set a release date.',
-        link: 'https://muffinman.io/skyblue/',
-        name: 'SkyBlue',
+          "Small CSS framework I created for my personal needs, a decade ago. Designed as a lightweight alternative to frameworks like Bootstrap. I started working on v2 recently, but I have not set a release date.",
+        link: "https://muffinman.io/skyblue/",
+        name: "SkyBlue",
       },
     ],
   },
   {
-    title: 'Everything else',
+    title: "Everything else",
     projects: [
       {
         description:
-          'Very personal project - a web adaptation of the book *Letters From Sarajevo*. Our father was stuck in Sarajevo during the war and still sent us funny, positive letters filled with drawings.',
-        image: './letters.png',
-        link: 'https://lettersfromsarajevo.com/',
-        name: 'Letters from Sarajevo',
+          "A tiny, hand-crafted book I made my wife using AI, a pen plotter and 3D printer.",
+        image: "./tiny-book.png",
+        link: "/blog/the-tiny-book-of-great-joys/",
+        name: "The Tiny Book of Great Joys",
       },
       {
         description:
-          'A tiny, hand-crafted book I made my wife using AI, a pen plotter and 3D printer.',
-        image: './tiny-book.png',
-        link: '/blog/the-tiny-book-of-great-joys/',
-        name: 'The Tiny Book of Great Joys',
+          "Very personal project - a web adaptation of the book *Letters From Sarajevo*. Our father was stuck in Sarajevo during the war and still sent us funny, positive letters filled with drawings.",
+        image: "./letters.png",
+        link: "https://lettersfromsarajevo.com/",
+        name: "Letters from Sarajevo",
+      },
+      {
+        description:
+          "My first game. A simple shoot-em-up prototype with a twist at the end.",
+        image: "./ftf.png",
+        link: "https://muffinman-io.itch.io/ftf",
+        name: "FTF",
       },
       {
         description:
           "Small code experiments made for Work&Co's weekly code challenge.",
-        image: './code-challenge.png',
-        link: 'https://muffinman.io/weekly-code-challenge/',
-        name: 'Weekly Code Challenge',
+        image: "./code-challenge.png",
+        link: "https://muffinman.io/weekly-code-challenge/",
+        name: "Weekly Code Challenge",
       },
     ],
   },
@@ -323,8 +337,8 @@ const projects: {
 const Projects = () => {
   return (
     <BaseTemplate
-      outputDir={join(dirs.OUTPUT, 'projects')}
-      dirPath={join(dirs.CONTENT, 'projects')}
+      outputDir={join(dirs.OUTPUT, "projects")}
+      dirPath={join(dirs.CONTENT, "projects")}
       pathname="/projects/"
       description="List of selected projects I created."
       title="Projects"
@@ -357,8 +371,8 @@ const Projects = () => {
                         src={project.image}
                         alt={project.name}
                         className="project__image"
-                        outputDir={join(dirs.OUTPUT, 'projects')}
-                        pageDir={join(dirs.CONTENT, 'projects')}
+                        outputDir={join(dirs.OUTPUT, "projects")}
+                        pageDir={join(dirs.CONTENT, "projects")}
                       />
                     );
                   }
