e54a5690e44f8367d1da3af0c296ec3e7c8b9ab4
Stanko <stanko.tadic@gmail.com>
2025-09-24 11:23:31 +0200

    Updated post

--- site/components/comments.tsx
+++ site/components/comments.tsx
@@ -72,7 +72,7 @@ const Comments = ({ className = '', slug, theme, ...props }: CommentsProps) => {
       return Bun.TOML.parse(content) as Comment;
     })
     .sort((a, b) => {
-      return new Date(b.date).getTime() - new Date(a.date).getTime();
+      return new Date(a.date).getTime() - new Date(b.date).getTime();
     });
 
   return (
