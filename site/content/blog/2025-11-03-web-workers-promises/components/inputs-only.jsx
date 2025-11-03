e8b78e3692ef7020151b381400e7aae8f3a949be
Stanko <stanko.tadic@gmail.com>
2025-11-03 22:01:46 +0100

    New post, safari css fixes

---
+++ site/content/blog/2025-11-03-web-workers-promises/components/inputs-only.jsx
@@ -0,0 +1,30 @@
+import { Fragment } from "react";
+import clsx from "clsx";
+
+const Counter = ({ className, min = 1, max = 10, name }) => {
+  return (
+    <div className={clsx("inputs-only", className)}>
+      {new Array(max - min + 1).fill(0).map((_, index) => {
+        const value = index + min;
+        const id = `${name}-${value}`;
+        return (
+          <Fragment key={index}>
+            <input
+              type="radio"
+              id={id}
+              name={name}
+              value={value}
+              checked={index === 0}
+              onChange={() => {}} // dummy handler because React cries about it
+            />
+            <label htmlFor={id} key={index}>
+              {value}
+            </label>
+          </Fragment>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Counter;
