386e52372a7e136fafdf07c35fc5e12532d6eac8
Stanko <stanko.tadic@gmail.com>
2026-07-12 23:11:38 +0200

    Parallelized SASS compilation

--- app/lib/compile-sass.ts
+++ app/lib/compile-sass.ts
@@ -1,40 +1,49 @@
 import fs from 'node:fs';
 import { join } from 'node:path';
-import { compileAsync } from 'sass-embedded';
+import { initAsyncCompiler } from 'sass-embedded';
 import { dirs, IS_DEV } from './constants';
 import { log, paint } from '../utils/log';
 
+const compilerPromise = initAsyncCompiler();
+
 const processScssDir = async (dir: string, outputDir: string) => {
+  const compiler = await compilerPromise;
   const scssFiles = fs
     .readdirSync(dir)
     // Ignore _partials
     .filter((file) => file.endsWith('.scss') && !file.startsWith('_'));
 
-  for (const file of scssFiles) {
-    const filePath = join(dir, file);
-    const cssFilePath = join(
-      dirs.OUTPUT,
-      outputDir,
-      file.replace('.scss', '.css')
-    );
+  await Promise.all(
+    scssFiles.map(async (file) => {
+      const filePath = join(dir, file);
+      const cssFilePath = join(
+        dirs.OUTPUT,
+        outputDir,
+        file.replace('.scss', '.css')
+      );
 
-    const start = Date.now();
-    const result = await compileAsync(filePath, {
-      style: IS_DEV ? 'expanded' : 'compressed',
-      sourceMap: true,
-      // loadPaths: [dir],
-    });
+      const start = Date.now();
+      const result = await compiler.compileAsync(filePath, {
+        style: IS_DEV ? 'expanded' : 'compressed',
+        sourceMap: true,
+        // loadPaths: [dir],
+      });
 
-    Bun.write(cssFilePath, result.css);
-    Bun.write(`${cssFilePath}.map`, JSON.stringify(result.sourceMap));
+      await Promise.all([
+        Bun.write(cssFilePath, result.css),
+        Bun.write(`${cssFilePath}.map`, JSON.stringify(result.sourceMap)),
+      ]);
 
-    log.verbose(paint.blue('sass:'), `${file} [${Date.now() - start}ms]`);
-  }
+      log.verbose(paint.blue('sass:'), `${file} [${Date.now() - start}ms]`);
+    })
+  );
 };
 
 export const compileSASS = async () => {
   const start = Date.now();
-  await processScssDir(dirs.SCSS, 'css');
-  await processScssDir(join(dirs.SCSS, 'posts'), join('css', 'posts'));
+  await Promise.all([
+    processScssDir(dirs.SCSS, 'css'),
+    processScssDir(join(dirs.SCSS, 'posts'), join('css', 'posts')),
+  ]);
   log.info(paint.blue('sass:'), `compiled sass [${Date.now() - start}ms]`);
 };
